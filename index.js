43fb53a883e9cf06c6ad544371f5a66a46978582
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-18 21:25:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function theBeatlesPlay(){
+  var musicians ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  var instruments ["rhythm guitar", "bass guitar", "lead guitar", "drums"];
+  var i;
+  for (var i = 0; i < musicians.length; i++) {
+    musicians[i] + " plays " + instruments[i];
+  }
+}
+
+function johnLennonFacts()
+
+function iLoveTheBeatles()
