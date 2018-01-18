5d6367ee307948cf7058316e60072251103114b5
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-18 22:35:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
-  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
-  var instruments = ["rhythm guitar", "bass guitar", "lead guitar", "drums"]
-  var x = []
+  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  var instruments = ["rhythm guitar", "bass guitar", "lead guitar", "drums"];
+  var x = [];
   for (var i = 0; i < musicians.length; i++) {
     x[i] = musicians[i] + " plays " + instruments[i]
   }
