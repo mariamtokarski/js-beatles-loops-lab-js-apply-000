73e7643a48ac18115c70c0a657931e46cdf721c7
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-18 22:40:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["rhythm guitar", "bass guitar", "lead guitar", "drums"];
+
 function theBeatlesPlay(musicians, instruments){
-  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  var instruments = ["rhythm guitar", "bass guitar", "lead guitar", "drums"];
   var x = [];
   for (var i = 0; i < musicians.length; i++) {
     x[i] = musicians[i] + " plays " + instruments[i]
@@ -8,8 +10,9 @@ function theBeatlesPlay(musicians, instruments){
   return x
 }
 
+var facts ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
+
 function johnLennonFacts(facts){
-  var facts ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
   var x = []
   var i = 0
   while (facts[i]) {
