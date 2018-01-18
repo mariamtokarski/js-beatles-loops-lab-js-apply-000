1f49f749147ecd6f0ca3d3551c11e8f96614cb03
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-18 22:29:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 function johnLennonFacts(facts){
-  
+  var facts ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
+  var x = []
+  var i = 0
+  while (facts[i]) {
+    x[i] = facts[i] + "!!!"
+    i++
+  }
+  return x
 }
 
 function iLoveTheBeatles()
